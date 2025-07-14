16bb84e5886f003b2d757e206a55c8f905e9fbd8
Hibee-tech <deanhorlar@gmail.com>
2025-07-14 08:46:36 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header';
-//import MainContent from './components/MainContent';
-//import Footer from './components/Footer';
-//import WelcomeMessage from './components/WelcomeMessage';
-//import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
