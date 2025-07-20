0bd6fab80d13666b06f24cccb03ae7a2e363ca54
hi bee <deanhorlar@gmail.com>
2025-07-20 19:09:13 +0200

    Move UserContext.js into components folder

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,4 +1,10 @@
 import { createContext } from "react";
 
+ HEAD
 const UserContext = createContext(null); // Create the context
 export default UserContext;              // Export it for other components
+
+const UserContext = createContext(null);  // Create a context object
+
+export default UserContext;               // Export it for use in other components
+ d4c483d (Move UserContext.js into components folder)
