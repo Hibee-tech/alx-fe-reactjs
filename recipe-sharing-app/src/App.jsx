df19510adf9dbfda4a7f0daf032e88b9cb294768
hi bee <deanhorlar@gmail.com>
2025-07-26 21:17:40 +0200

    Fix: Add BrowserRouter to App.jsx for routing setup

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <>
+       <BrowserRouter> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,7 +25,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-
           <Routes>
         <Route path="/" element={
           <>
@@ -41,6 +41,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       </BrowserRouter>
     </>
   )
 }
