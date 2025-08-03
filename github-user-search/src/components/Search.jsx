47900791a988770c6aa118bdb100a017d68e645d
hi bee <deanhorlar@gmail.com>
2025-08-03 16:29:34 +0200

    Fix error message to match required string

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const SearchBar = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch (err) {
-      setError('Looks like we can’t find the user.');
+      setError('Looks like we cant find the user.');
     } finally {
       setLoading(false);
     }
