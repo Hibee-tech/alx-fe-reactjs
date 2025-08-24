8de59fb72c5a78fe523e3ad63320ae933f121c91
hi bee <deanhorlar@gmail.com>
2025-08-24 16:01:31 +0200

    Implement advanced data fetching with React Query (PostsComponent)

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,16 @@
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./PostsComponent";
 import './App.css'
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
+     <QueryClientProvider client={queryClient}>
        <PostsComponent />
+          </QueryClientProvider>
     </>
   )
 }
